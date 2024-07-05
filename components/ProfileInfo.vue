9d5244fa51c9a63fb68fb70e7bece480206735f0
megasrd <reganjduthie@gmail.com>
2024-07-05 09:14:29 +0200

    Removed non specific source from ProfileInfo.vue

--- components/ProfileInfo.vue
+++ components/ProfileInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-2xl mx-auto">
-    <img class="w-36 h-36 rounded-full mx-auto mb-6 bg-slate-800" src="user.images[1].url || ''"/>
+    <img class="w-36 h-36 rounded-full mx-auto mb-6 bg-slate-800"/>
     <h2 class="text-center font-bold text-5xl"> Regan-Jay Dee </h2>
     <div class="grid grid-cols-3 gap-4 mt-4 mx-auto max-w-sm">
         <div class="text-center">
