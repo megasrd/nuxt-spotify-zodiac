88aee674cc4044ddbc9b9a6dcf0ce98a3a6387e0
megasrd <reganjduthie@gmail.com>
2024-10-18 09:40:49 +0200

    Updated ProfileInfo Component to show higher res image

--- components/ProfileInfo.vue
+++ components/ProfileInfo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-2xl mx-auto">
-    <img class="md:w-48 md:h-48 w-36 h-36 rounded-full mx-auto mb-6 bg-slate-800" :src="user.images[1].url">
+    <img class="md:w-48 md:h-48 w-36 h-36 rounded-full mx-auto mb-6 bg-slate-800" :src="user.images[0].url">
     <h2 class="md:text-center font-bold md:text-4xl text-3xl"> Welcome, {{ user.display_name }}. </h2>
     <!-- <div class="grid grid-cols-3 gap-4 mt-4 mx-auto max-w-sm">
         <div class="text-center">
